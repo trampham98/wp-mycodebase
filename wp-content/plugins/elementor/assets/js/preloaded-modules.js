a06a59b3b447370768fdf7252785273e1e8a5800
Tram Pham <tram.pham@madison-technology.com>
2022-10-03 11:53:05 +0700

    Update WP & plugins to the lastest version

--- wp-content/plugins/elementor/assets/js/preloaded-modules.js
+++ wp-content/plugins/elementor/assets/js/preloaded-modules.js
@@ -1,4 +1,4 @@
-/*! elementor - v3.7.0 - 08-08-2022 */
+/*! elementor - v3.7.8 - 02-10-2022 */
 (self["webpackChunkelementor"] = self["webpackChunkelementor"] || []).push([["preloaded-modules"],{
 
 /***/ "../assets/dev/js/frontend/handlers/accordion.js":
